193c4ae6c28f6b882fff602e95febf6f77492e79
Андрій Свирида <andrijsvirida2@gmail.com>
2024-12-02 00:48:41 +0200

    mock claims

--- Test.Data/UsersData.cs
+++ Test.Data/UsersData.cs
@@ -5,5 +5,5 @@ namespace Tests.Common;
 public static class UsersData
 {
     public static User MainUser(string password)
-        => User.New(UserId.New(), "Username", "test@example.com", password, UserRole.Admin);
+        => User.New(new UserId(new Guid("6d2734ee-e084-4fa9-b8e7-4a7389f9f5cb")), "Username", "test@example.com", password, UserRole.Admin);
 }
